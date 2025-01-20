508d7c7c38285f84387f0753902c3e8671f3fc47
Malik Sadiki-Torres <maliktorres98@gmail.com>
2025-01-20 16:50:42 -0500

    created an array for the cards and added a ForEach

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -9,14 +9,16 @@ import SwiftUI
 
 // This struct content view behaves(functionality) like a view
 struct ContentView: View {
+    // Alternate way of writing
+    // let emojis:[String] = ["👻","🎃","🕷️","😈"]
+    let emojis: Array<String> = ["👻","🎃","🕷️","😈"]
             // some View see what is being returned and ensures that it is some view
     var body: some View { // Computed Proberty
         // VStack function returns something that is a view
         HStack {
-            CardView(isFaceUp: false)
-            CardView()
-            CardView(isFaceUp: true)
-            CardView(isFaceUp: true)
+            ForEach(emojis.indices, id: \.self) { index in
+                CardView(content: emojis[index])
+            }
         }
         // View Modifier
         .foregroundColor(.orange)
@@ -25,13 +27,12 @@ struct ContentView: View {
     }
 }
 
-
-
 struct CardView: View {
+    let content: String
     // @State small change
     // Temporary state
     // @ creates a pointer to memory
-  @State var isFaceUp = false
+  @State var isFaceUp = true
     // all structs every var has to have a value
     var body: some View {
         ZStack {
@@ -40,7 +41,7 @@ struct CardView: View {
                 if isFaceUp {
                     base.fill(.white)
                     base.strokeBorder(lineWidth:2)
-                Text("👻").font(.largeTitle)
+                Text(content).font(.largeTitle)
             } else {
                 RoundedRectangle(cornerRadius:12)
             }
